2426e3209ab0c31ce152e2f4b49d66192de05770
Kishor978 <thaunnakishor@gmail.com>
2023-12-08 16:25:37 +0545

    parameter tuining completed

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from src.excecption import CustomExcecption
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -18,11 +19,17 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomExcecption
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param=param[list(models.keys())[i]]
+
+            gs=GridSearchCV(model,param,cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train) # Train model
 
             # Make predictions
